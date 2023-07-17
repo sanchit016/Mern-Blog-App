35571cd72cb12831712b07a16ff9a59eb9c560bb
Sanchit <98282778+sanchit016@users.noreply.github.com>
2023-07-17 17:21:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.md']
+  assetsInclude: ['**/*.md'],
+  css: {
+    modules: true, // Enable CSS modules if needed
+  },
 })
