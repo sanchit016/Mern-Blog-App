54c5649b615c75161716b7886b51399535af4e15
Sanchit <98282778+sanchit016@users.noreply.github.com>
2023-07-18 00:55:25 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.md'],
   css: {
-    modules: true, 
+    modules: true,
   },
   server: {
-    proxy: {
-      '/api': {
+    middleware: [
+      createProxyMiddleware('/api', {
         target: 'https://mern-blog-app-vvpv.onrender.com',
         changeOrigin: true,
+      }),
+      (req, res, next) => {
+        // Redirect all requests to index.html
+        if (req.originalUrl !== '/' && !req.originalUrl.includes('.')) {
+          req.url = '/';
+        }
+        next();
       },
-    },
-    middleware: (req, res, next) => {
-      // Redirect all requests to index.html
-      if (req.url !== '/' && !req.url.includes('.')) {
-        req.url = '/';
-      }
-      next();
-    },
+    ],
   },
-})
+});
